a2491c2dae9fdff8bb720c98895c59c0f54168e1
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-08-03 16:17:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import telebot
-from keep_alive import keep_alive
-import os
-keep_alive()
+from flask import Flask
 
 
-# TOKEN = '6921639698:AAGgvNBVhXqAaXQxpyAA6NL03zoIcG8yxY4'
+app = Flask(__name__)
 
-bot = telebot.TeleBot(token = os.environ.get("token"))
 
-# تعریف دستور /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "سلام من عبدالله چلاسی هستم")
+@app.route('/')
+def index():
+    return "ABDOLLAH CHELASI"
 
-# تعریف دستور /help
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, "راهنما: این ربات پیام‌های شما را در چت تلگرام نمایش می‌دهد.")
 
-# پاسخ به پیام‌های متنی
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-# اجرای ربات
-bot.polling()
+if __name__ == "__main__":
+    app.run()
