0e5886aef3caff584d10e444e8a4f146e0967245
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-08-03 17:07:00 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+# from flask import Flask
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "ABDOLLAH CHELASI"
+# @app.route('/')
+# def index():
+#     return "ABDOLLAH CHELASI"
 
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+import streamlit as st
+
+st.warning("ABDOLLAH CH")
